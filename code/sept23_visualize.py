592f3fa1d336df1295ce2193652b913a6d422a5a
zyzdiana <yingzhuozhang@g.harvard.edu>
2015-10-22 21:14:03 -0400

    minor update

--- code/sept23_visualize.py
+++ code/sept23_visualize.py
@@ -158,7 +158,7 @@ def error_in_time(cost_dict, res, step_size, axes, ax_to_idx_dict=ax_to_idx):
             plt.ylabel('Absolute error')
     plt.show()
 
-def error_plot(cost_dict, col, res, step_size, axes, ax_to_idx_dict=ax_to_idx,theta_range = 1):
+def error_plot(cost_dict, col, res, step_size, axes, ax_to_idx_dict=ax_to_idx,theta_range = 1,xlim = [0,7]):
     colors = ['red','blue','green','orange','brown','purple']
     ranges = ['0_5_to_2_5','3_0_to_5_0']
     errors = []
@@ -176,12 +176,12 @@ def error_plot(cost_dict, col, res, step_size, axes, ax_to_idx_dict=ax_to_idx,th
                     y = abs(thetas[np.argmin(cost,axis=0)])
                     plt.scatter(col,deg-y, lw=0,s = 30, c = colors[ax_to_idx[rot_ax]],alpha = 0.1,marker='o')
             #plt.hlines([0.05,-0.05],-5,40,'black')
-            plt.xlim([0,7])
+            plt.xlim(xlim)
             plt.ylim([-0.5,3.5])
             #plt.title('%s, %s, rot_%s, trans_%s' % (res,rot_range,rot_ax,axes_dict[rot_ax]))
-            plt.title('Error Plot for Rotations')
+            plt.title('Error Plot for Rotations',fontsize=20)
             #plt.xlabel('Data Set')
-            plt.ylabel('Errors')
+            plt.ylabel('Errors (degrees)',fontsize=15)
     #plt.show()
 #########################################################
 # Plot Tricubic Results
@@ -196,30 +196,38 @@ def preprocess(cost_dict1,cost_dict2):
         cost_dict.append(dict_)
     return cost_dict
 
-def load_pickle(res,rotation):
-    filename1 = '/Users/zyzdiana/Dropbox/THESIS/sept23_tricubic_%s_%s_rotation_001_0.p' % (res, rotation)
+def load_pickle(res,rotation,path = '/Users/zyzdiana/Dropbox/THESIS/'):
+    filename1 = path + 'sept23_tricubic_%s_%s_rotation_001_0.p' % (res, rotation)
     cost_dict_1 = pickle.load(open(filename1,'rb'))
-    filename2 = '/Users/zyzdiana/Dropbox/THESIS/sept23_tricubic_%s_%s_rotation_001_1.p' % (res, rotation)
+    filename2 = path + 'sept23_tricubic_%s_%s_rotation_001_1.p' % (res, rotation)
     cost_dict_2 = pickle.load(open(filename2,'rb'))
     return preprocess(cost_dict_1,cost_dict_2)
 
-def split_plot(cost_dict_1, cost_dict_2,res,ax_to_idx_dict):
+def split_plot(cost_dict_1, cost_dict_2,cost_dict_1_shifted, cost_dict_2_shifted,cost_dict_1_filtered, cost_dict_2_filtered,res,ax_to_idx_dict):
     # Split the axes to see the results more clearly
     plt.figure(figsize = [18,12])
     for idx, ax in enumerate(['x','y','z']):
         plt.subplot(2,3,idx+1)
         axes = [ax]
-        scatter_plot_cubic(cost_dict_1, res, axes,0.01,ax_to_idx_dict)
+        scatter_plot_cubic(cost_dict_1, res, axes,0.01,ax_to_idx_dict,alpha = 0.4,s=50)
+        scatter_plot_cubic(cost_dict_1_shifted, res, axes,0.01,ax_to_idx_dict,alpha = 0.4,s = 100,marker='v',
+            label_arg = ' shifted center',colors = ['orange','brown','purple','red','blue','green'])
+        scatter_plot_cubic(cost_dict_1_filtered, res, axes,0.01,ax_to_idx_dict,alpha = 0.4,s = 100,marker='*',
+            label_arg = ' filtered',colors = ['red','purple','orange','brown','green','blue'])
     plt.show()
     plt.figure(figsize = [18,12])
     for idx, ax in enumerate(['xy','yz','xz']):
         plt.subplot(2,3,idx+1)
         axes = [ax]
-        scatter_plot_cubic(cost_dict_2, res, axes,0.01,ax_to_idx_dict)
+        scatter_plot_cubic(cost_dict_2, res, axes,0.01,ax_to_idx_dict,alpha = 0.4,s=50)
+        scatter_plot_cubic(cost_dict_2_shifted, res, axes,0.01,ax_to_idx_dict,alpha = 0.4,s = 100, marker='v',
+            label_arg = ' shifted center',colors = ['orange','brown','purple','red','blue','green'])
+        scatter_plot_cubic(cost_dict_2_filtered, res, axes,0.01,ax_to_idx_dict,alpha = 0.4,s = 100,marker='*',
+            label_arg = ' filtered',colors = ['red','purple','orange','brown','green','blue'])
     plt.show()   
 
-def scatter_plot_cubic(cost_dict, res, axes, step_size=0.01, ax_to_idx_dict=ax_to_idx):
-    colors = ['red','blue','green','orange','brown','purple']
+def scatter_plot_cubic(cost_dict, res, axes, step_size=0.01, ax_to_idx_dict=ax_to_idx,alpha = 0.6, s = 40, marker = 'o',label_arg = '',colors = ['red','blue','green','orange','brown','purple']):
+    #colors = ['red','blue','green','orange','brown','purple']
     ranges = ['0_5_to_2_5','3_0_to_5_0']
     count = 0
     for idx, rot_ax in enumerate(axes):
@@ -230,7 +238,7 @@ def scatter_plot_cubic(cost_dict, res, axes, step_size=0.01, ax_to_idx_dict=ax_t
                 for i in xrange(1,6):
                     rep = i + loop * 6
                     if(count == 1): 
-                        label = rot_ax
+                        label = rot_ax+label_arg
                         count = 0
                     else: label = None
                     rot_angle = rep_to_angle(rep,rot_range)
@@ -238,14 +246,14 @@ def scatter_plot_cubic(cost_dict, res, axes, step_size=0.01, ax_to_idx_dict=ax_t
                     thetas = np.arange(deg-1,deg+1,step_size)
                     cost = cost_dict[ax_to_idx_dict[rot_ax]][rot_angle]
                     y = abs(thetas[np.argmin(cost,axis=0)])
-                    plt.scatter(deg, y, lw=0,s = 30, c = colors[ax_to_idx[rot_ax]],alpha = 0.6,marker='o',label = label)
+                    plt.scatter(deg, y, lw=0,s = s, c = colors[ax_to_idx[rot_ax]],alpha = alpha,marker=marker,label = label)
     plt.plot([0,6],[0,6],c='black')
     plt.xlim([0,6])
     plt.ylim([0,6])
     plt.legend(loc=4)
-    plt.xlabel('True Rotations (Degrees)')
-    plt.ylabel('Search Results from Registration')
-    plt.title('Search Results for %s Resolution' %res)
+    plt.xlabel('True Rotations (Degrees)', fontsize=15)
+    plt.ylabel('Search Results from Registration (Degrees)', fontsize = 15)
+    plt.title('Search Results for %s Resolution' %res, fontsize=18)
     
 def scatter_plot_all_cubic(axes, cost_dict_10, cost_dict_8, cost_dict_6_4,figsize = [12,4],step_size = 0.01):
     plt.figure(figsize = figsize)
@@ -283,14 +291,14 @@ def scatter_plot_trans(cost_dict, res, axes):
                     step = rot_angle[1]
                     cost = cost_dict[rot_ax][rot_angle]
                     y = abs(trans[np.argmin(cost,axis=0)])*float(res[:-2])
-                    plt.scatter(step, y, s = 80, c = colors[ax_to_idx[rot_ax]],alpha = 0.3,marker='o',lw=0,label = label)
+                    plt.scatter(step, y, s = 60, c = colors[ax_to_idx[rot_ax]],alpha = 0.3,marker='o',lw=0,label = label)
     plt.plot([0,6],[0,6],c='black')
     plt.xlim([0,6])
     plt.ylim([0,6])
     plt.legend(loc='best')
-    plt.xlabel('True Translations (mm)')
-    plt.ylabel('Search Results from Registration')
-    plt.title('Search Results for %s Resolution' %res)
+    plt.xlabel('True Translations (mm)', fontsize = 15)
+    plt.ylabel('Search Results from Registration (mm)', fontsize = 15)
+    plt.title('Search Results for %s Resolution' %res, fontsize = 15)
     
 def scatter_plot_all_trans(axes, cost_dict_10, cost_dict_8, cost_dict_6_4,figsize = [12,4], interp = 'Linear'):
     plt.figure(figsize = figsize)
@@ -300,7 +308,7 @@ def scatter_plot_all_trans(axes, cost_dict_10, cost_dict_8, cost_dict_6_4,figsiz
     scatter_plot_trans(cost_dict_8,'8mm',axes)
     plt.subplot(1,3,3)
     scatter_plot_trans(cost_dict_6_4,'6.4mm',axes)
-    plt.suptitle('%s Interpolation'% interp)
+    #plt.suptitle('%s Interpolation'% interp, fontsize = 20)
     plt.show() 
 
 def error_in_time_trans(cost_dict, res):
@@ -344,7 +352,7 @@ def error_plot_trans(cost_dict, col, res):
             plt.xlim([0,7])
             plt.ylim([-2.0,1.0])
             #plt.title('%s, %s, rot_%s, trans_%s' % (res,rot_range,rot_ax,axes_dict[rot_ax]))
-            plt.title('Error Plot for Translations')
+            plt.title('Error Plot for Translations',fontsize=20)
             #plt.xlabel('Data Set')
-            plt.ylabel('Errors')
+            plt.ylabel('Errors (mm)',fontsize=15)
     #plt.show()
