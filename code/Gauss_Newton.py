59c7c506afc75dc4456e7407bcdfab0dc1cea389
zyzdiana <yingzhuozhang@g.harvard.edu>
2015-11-25 15:24:06 -0500

    recovered from backup

--- code/Gauss_Newton.py
+++ code/Gauss_Newton.py
@@ -15,59 +15,44 @@ from visualize import plot_cost_func
 from cost_functions import cf_ssd
 from mask import sphere_mask
 
-# Function to compute the derivatives for the target volume
 def axis_derivatives(volume):
     shape = volume.shape
-    tricubic_derivative_dict = {}
-    for i in xrange(0,shape[0]):
-        for j in xrange(0,shape[1]):
-            for k in xrange(0,shape[2]):
-                # Take care of boundary conditions
-                x1 = i
-                y1 = j
-                z1 = k
-                x0 = x1 - 1
-                x2 = x1 + 1
-                y0 = y1 - 1
-                y2 = y1 + 1
-                z0 = z1 - 1
-                z2 = z1 + 1
-                
-#                     # Wrap Around
-#                 x0 = (x0 + volume.shape[1]) % volume.shape[1]
-#                 x2 = (x2 + volume.shape[1]) % volume.shape[1]
-#                 y0 = (y0 + volume.shape[0]) % volume.shape[0]
-#                 y2 = (y2 + volume.shape[0]) % volume.shape[0]
-#                 z0 = (z0 + volume.shape[2]) % volume.shape[2]
-#                 z2 = (z2 + volume.shape[2]) % volume.shape[2] 
+    xx,yy,zz = np.meshgrid(np.linspace(0,shape[1]-1,shape[1]),np.linspace(0,shape[0]-1,shape[0]),np.linspace(0,shape[2]-1,shape[2]))
+    x1 = xx.astype(int)
+    y1 = yy.astype(int)
+    z1 = zz.astype(int)
 
-#                 # Wrap around original grid points
-#                 x1 = (x1 + volume.shape[1]) % volume.shape[1]
-#                 y1 = (y1 + volume.shape[0]) % volume.shape[0]
-#                 z1 = (z1 + volume.shape[2]) % volume.shape[2]
-                
-                x0 = np.clip(x0, 0, volume.shape[1]-1)
-                x1 = np.clip(x1, 0, volume.shape[1]-1)
-                x2 = np.clip(x2, 0, volume.shape[1]-1)
-                y0 = np.clip(y0, 0, volume.shape[0]-1)
-                y1 = np.clip(y1, 0, volume.shape[0]-1)
-                y2 = np.clip(y2, 0, volume.shape[0]-1)
-                z0 = np.clip(z0, 0, volume.shape[2]-1)
-                z1 = np.clip(z1, 0, volume.shape[2]-1)
-                z2 = np.clip(z2, 0, volume.shape[2]-1)
+    x0 = x1 - 1
+    x2 = x1 + 1
+    y0 = y1 - 1
+    y2 = y1 + 1
+    z0 = z1 - 1
+    z2 = z1 + 1
 
-                # Compute vector Y from known points
-                Y = np.zeros([3,])
+    # wrap around
+    x0 = (x0 + shape[1]) % shape[1]
+    x2 = (x2 + shape[1]) % shape[1]
+    y0 = (y0 + shape[0]) % shape[0]
+    y2 = (y2 + shape[0]) % shape[0]
+    z0 = (z0 + shape[2]) % shape[2]
+    z2 = (z2 + shape[2]) % shape[2] 
 
-                # values of df/dx at each corner.
-                Y[1] = ((volume[y1,x2,z1]-volume[y1,x0,z1])/2.)
-                # values of df/dy at each corner.
-                Y[0] = ((volume[y2,x1,z1]-volume[y0,x1,z1])/2.)
-                # values of df/dz at each corner.
-                Y[2] = ((volume[y1,x1,z2]-volume[y1,x1,z0])/2.)
+    # Wrap around original grid points
+    x1 = (x1 + shape[1]) % shape[1]
+    y1 = (y1 + shape[0]) % shape[0]
+    z1 = (z1 + shape[2]) % shape[2]
 
-                tricubic_derivative_dict[(i,j,k)] = Y
-    return tricubic_derivative_dict
+    # Compute vector Y from known points
+    Y = np.empty([shape[1],shape[0],shape[2],3])
+
+    # values of df/dx at each corner.
+    Y[:,:,:,1] = ((volume[y1,x2,z1]-volume[y1,x0,z1])/2.)
+    # values of df/dy at each corner.
+    Y[:,:,:,0] = ((volume[y2,x1,z1]-volume[y0,x1,z1])/2.)
+    # values of df/dz at each corner.
+    Y[:,:,:,2] = ((volume[y1,x1,z2]-volume[y1,x1,z0])/2.)
+
+    return Y
 
 def rotate_coords_transformation_m(x, y, z, params, ox,oy,oz, k=16.):
     l = np.sqrt(params[3]**2+params[4]**2+params[5]**2)/k
@@ -84,10 +69,7 @@ def rotate_coords_transformation_m(x, y, z, params, ox,oy,oz, k=16.):
     rotz = (alpha**2-beta**2-gamma**2+delta**2)*z+2*(gamma*delta+alpha*beta)*y+2*(beta*delta-alpha*gamma)*x+oz+params[2]
 
     return (rotx,roty,rotz)
-<<<<<<< HEAD
-=======
 
->>>>>>> 8e09c006c31cc8b033bb57685a3ea72162f85b7e
 def get_M(x1_org,x2_org,x3_org,k=16.):
     x1 = x1_org/k
     x2 = x2_org/k
@@ -102,14 +84,16 @@ def trace_plot(Ps,res):
     arr = np.array(Ps)
     plt.figure(figsize = [12,4])
     plt.subplot(1,2,1)
+    t_legends = ['x','y','z']
     for i in xrange(3):
-        plt.plot(arr[:,i]*res,label = 't_%s'%i)
+        plt.plot(arr[:,i]*res,label = 't_%s'%t_legends[i])
     plt.legend(loc = 'best')
     plt.xlabel('Iterations', fontsize = 15)
     plt.ylabel('Translations (mm)', fontsize = 15)
     plt.subplot(1,2,2)
+    R_legends = ['x','y','z']
     for j in xrange(3,6):
-        plt.plot(to_degree(arr[:,j]),label = 'R_%s'%j)
+        plt.plot(to_degree(arr[:,j]),label = 'R_%s'%R_legends[j-3])
     plt.legend(loc = 'best')
     plt.suptitle('Trace Plot for the Transformation Parameters',fontsize=18)
     plt.xlabel('Iterations', fontsize = 15)
@@ -136,14 +120,16 @@ def print_results(errors, Ps, res):
 
 def Gauss_Newton(Vol1, Vol1_derivatives, Vol2, Vol2_derivatives, 
                  divide_factor = 16., alpha = 0.2, decrease_factor = 0.25, 
-                 P_initial = np.array([0,0,0,0,0,0]), plot = True, max_iter = 20):
+                 P_initial = np.array([0,0,0,0,0,0]), plot = True, max_iter = 20, mask = False):
 
     volume_shape = Vol1.shape
     if (volume_shape[0] == 26): res = '10'
     if (volume_shape[0] == 32): res = '8'
     if (volume_shape[0] == 40): res = '6_4'
-    rad = res_to_rad(res)
+    rad = volume_shape[0]/2
     xx,yy,zz = pickle.load(open('/Users/zyzdiana/Dropbox/THESIS/for_cluster/mesh_grid_%s.p'%res,'rb'))
+    #if(mask):
+        #Vol1 = sphere_mask(Vol1,rad)
     ox = volume_shape[1]/2.-0.5
     oy = volume_shape[0]/2.-0.5
     oz = volume_shape[2]/2.-0.5
@@ -167,17 +153,21 @@ def Gauss_Newton(Vol1, Vol1_derivatives, Vol2, Vol2_derivatives,
         Jr_rP = np.zeros([6,])
         dest = np.empty(volume_shape)
         idx = 0
+        #Mtensor = np.empty([volume_shape[0],volume_shape[0],volume_shape[0],3,6])
         for i in xrange(volume_shape[0]):
             for j in xrange(volume_shape[1]):
                 for k in xrange(volume_shape[2]):
-                    dest[i,j,k] = tricubic_interp(volume_shape,Vol2_derivatives,dest_x[i,j,k],dest_y[i,j,k],dest_z[i,j,k]) 
+                    dest[i,j,k] = tricubic_interp(volume_shape,Vol2_derivatives,dest_x[i,j,k],dest_y[i,j,k],dest_z[i,j,k])
                     M = get_M(yy[i,j,k]-ox,xx[i,j,k]-oy,zz[i,j,k]-oz,divide_factor)
                     for ii in xrange(len(P_old)):
-                        Jr[idx,ii] = -1.*Vol1_derivatives[j,i,k].dot(M[:,ii])
-                        Jr_rP[ii] += Jr[idx,ii]*(Vol1[yy[i,j,k],xx[i,j,k],zz[i,j,k]]-dest[i,j,k])
+                        Jr[idx,ii] = -1.*Vol1_derivatives[i,j,k].dot(M[:,ii])
                     idx += 1
-
-        error = cf_ssd(Vol1,sphere_mask(dest,rad))
+        if(mask):
+            dest = sphere_mask(dest,rad)
+        flatR = np.ravel(Vol1-dest)
+        Jr_rP = Jr.T.dot(flatR)
+        
+        error = cf_ssd(Vol1,dest)
 
         ## if error is getting larger, go back one step and decrease alpha
         if(error > errors[-1]):
